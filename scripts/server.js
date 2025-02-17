a71c1dc7d5fd2b972d520b45a1efb5f4df0ac69a
leavittm <markus.leavitt@gmail.com>
2025-02-17 18:52:14 -0500

    node modules installed

---
+++ scripts/server.js
@@ -0,0 +1,51 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+
+const MIME_TYPES = {
+    '.html': 'text/html',
+    '.css': 'text/css',
+    '.js': 'text/javascript',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.jpeg': 'image/jpeg',
+    '.gif': 'image/gif'
+};
+
+const server = http.createServer((req, res) => {
+    // Handle root path
+    let filePath = req.url === '/' ? './index.html' : '.' + req.url;
+    
+    // Get the file extension
+    const ext = path.extname(filePath);
+    
+    // Set the content type
+    const contentType = MIME_TYPES[ext] || 'text/plain';
+
+    // Read the file
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                // Page not found
+                fs.readFile('./404.html', (err, content) => {
+                    res.writeHead(404, { 'Content-Type': 'text/html' });
+                    res.end(content, 'utf8');
+                });
+            } else {
+                // Server error
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`);
+            }
+        } else {
+            // Success
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf8');
+        }
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}/`);
+}); 
